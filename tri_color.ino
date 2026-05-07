00f4a83a6333cd779538f6bb696011b77d8a2ac3
ghostrider635 <aron.kibalikwa@gmail.com>
2026-05-07 22:58:49 +0100

    Refactor du code de détection de couleur : amélioration de la lecture des fréquences et ajout de nouvelles nuances

--- tri_color.ino
+++ tri_color.ino
@@ -1,116 +1,111 @@
-// Pins TCS3200
+// --- Configuration des Pins (Inchangée) ---
 #define S0 18
 #define S1 19
 #define S2 21
 #define S3 22
 #define sensorOut 23
-
-// Pins LED RGB
 #define RED_LED 25
 #define GREEN_LED 26
 #define BLUE_LED 27
 
-int redFrequency = 0;
-int greenFrequency = 0;
-int blueFrequency = 0;
+// --- Paramètres d'étalonnage (Ajustés selon vos tests) ---
+// Ces valeurs permettent de convertir la fréquence en 0-255
+int redMin = 15,   redMax = 220; 
+int greenMin = 15, greenMax = 220;
+int blueMin = 15,  blueMax = 220;
 
 void setup() {
   Serial.begin(115200);
 
-  // Capteur
+  // Configuration Capteur
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(sensorOut, INPUT);
 
-  // LED RGB
+  // Configuration LED (Sorties PWM)
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
 
-  // Mise à l'échelle fréquence à 20%
+  // Échelle de fréquence à 20%
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
 
-  eteindreLED();
+  Serial.println("Système prêt : Détection de toutes les couleurs...");
 }
 
 void loop() {
+  // 1. LECTURE des fréquences brutes
+  int rBrut = readFrequency(LOW, LOW);   // Filtre Rouge
+  delay(10);                             // Petite pause pour la stabilité
+  int gBrut = readFrequency(HIGH, HIGH); // Filtre Vert
+  delay(10);
+  int bBrut = readFrequency(LOW, HIGH);  // Filtre Bleu
 
-  // Lire rouge
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, LOW);
-  redFrequency = pulseIn(sensorOut, LOW);
+  // 2. CONVERSION (Mapping) en échelle 0-255
+  // On utilise constrain pour éviter de dépasser 255 ou descendre sous 0
+  int R = constrain(map(rBrut, redMin, redMax, 255, 0), 0, 255);
+  int G = constrain(map(gBrut, greenMin, greenMax, 255, 0), 0, 255);
+  int B = constrain(map(bBrut, blueMin, blueMax, 255, 0), 0, 255);
 
-  // Lire vert
-  digitalWrite(S2, HIGH);
-  digitalWrite(S3, HIGH);
-  greenFrequency = pulseIn(sensorOut, LOW);
+  // 3. AFFICHAGE des données pour contrôle
+  Serial.printf("RGB: [%d, %d, %d] | Bruts: (r:%d, g:%d, b:%d)\n", R, G, B, rBrut, gBrut, bBrut);
+  
+  // 4. ACTIONS
+  identifierNuance(R, G, B);
+  piloterLED(R, G, B);
 
-  // Lire bleu
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, HIGH);
-  blueFrequency = pulseIn(sensorOut, LOW);
-
-  Serial.print("R: ");
-  Serial.print(redFrequency);
-
-  Serial.print(" G: ");
-  Serial.print(greenFrequency);
-
-  Serial.print(" B: ");
-  Serial.println(blueFrequency);
-
-  detecterCouleur();
-
-  delay(300);
+  delay(400); // Vitesse de lecture
 }
 
-void detecterCouleur() {
+// Fonction de lecture propre
+int readFrequency(int s2, int s3) {
+  digitalWrite(S2, s2);
+  digitalWrite(S3, s3);
+  return pulseIn(sensorOut, LOW);
+}
 
-  // Plus la valeur est basse, plus la couleur est détectée fortement
+// Commande de la LED par mélange de couleurs (PWM)
+void piloterLED(int r, int g, int b) {
+  analogWrite(RED_LED, r);
+  analogWrite(GREEN_LED, g);
+  analogWrite(BLUE_LED, b);
+}
 
-  if (redFrequency < greenFrequency && redFrequency < blueFrequency) {
-    Serial.println("ROUGE");
-    afficherRouge();
+// Analyse précise des nuances
+void identifierNuance(int r, int g, int b) {
+  int total = r + g + b;
+
+  if (total < 50) {
+    Serial.println(">> NOIR / TROP SOMBRE");
+  } 
+  else if (r > 210 && g > 210 && b > 210) {
+    Serial.println(">> BLANC / TRES CLAIR");
   }
-
-  else if (greenFrequency < redFrequency && greenFrequency < blueFrequency) {
-    Serial.println("VERT");
-    afficherVert();
+  else if (r > 200 && g > 100 && g < 180 && b < 100) {
+    Serial.println(">> ORANGE");
   }
-
-  else if (blueFrequency < redFrequency && blueFrequency < greenFrequency) {
-    Serial.println("BLEU");
-    afficherBleu();
+  else if (r > 180 && g > 180 && b < 120) {
+    Serial.println(">> JAUNE");
   }
-
+  else if (r > 150 && b > 150 && g < 130) {
+    Serial.println(">> VIOLET / ROSE");
+  }
+  else if (g > 150 && b > 150 && r < 130) {
+    Serial.println(">> TURQUOISE / CYAN");
+  }
+  else if (r > g && r > b) {
+    Serial.println(">> ROUGE");
+  }
+  else if (g > r && g > b) {
+    Serial.println(">> VERT");
+  }
+  else if (b > r && b > g) {
+    Serial.println(">> BLEU");
+  }
   else {
-    eteindreLED();
+    Serial.println(">> NUANCE MIXTE");
   }
-}
-
-void afficherRouge() {
-  digitalWrite(RED_LED, HIGH);
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(BLUE_LED, LOW);
-}
-
-void afficherVert() {
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(GREEN_LED, HIGH);
-  digitalWrite(BLUE_LED, LOW);
-}
-
-void afficherBleu() {
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(BLUE_LED, HIGH);
-}
-
-void eteindreLED() {
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(BLUE_LED, LOW);
 }
